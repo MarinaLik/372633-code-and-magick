29bf42f7c2700c37edfc264500119008ae0831cf
MarinaLik <mylym79@gmail.com>
2018-02-15 21:31:35 +0300

    Вынесены и инкапсулированы функции открытия и закрытия модального окна

---
+++ js/dialog.js
@@ -0,0 +1,40 @@
+'use strict';
+
+(function () {
+  var userDialog = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = userDialog.querySelector('.setup-close');
+  var wizardForm = userDialog.querySelector('.setup-wizard-form');
+
+  var onPopupEscPress = function (evt) {
+    if (wizardForm.elements.username !== document.activeElement) {
+      window.util.isEscEvent(evt, closePopup);
+    }
+  };
+
+  var openPopup = function () {
+    userDialog.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+  };
+
+  var closePopup = function () {
+    userDialog.classList.add('hidden');
+    document.removeEventListener('keydown', onPopupEscPress);
+  };
+
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+  });
+
+  setupOpen.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, openPopup);
+  });
+
+  setupClose.addEventListener('click', function () {
+    closePopup();
+  });
+
+  setupClose.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, closePopup);
+  });
+})();
